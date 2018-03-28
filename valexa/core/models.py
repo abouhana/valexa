Add template.xlsx. Fix behaviour of profiles calculation.

valexa/core/models.py
@@ -30,11 +30,11 @@ class Model:
 
     def handle_correction(self):
         recoveries = [(s.result / s.concentration) * 100 for s in self.series_calculated]
-        recovery_avg = np.mean(recoveries)
+        recovery_avg: float = np.mean(recoveries)
 
         if abs(100 - recovery_avg) >= 15:
             self.has_correction = True
-            self.correction_factor = 1 / (recovery_avg / 100)
+            self.correction_factor = round(1 / (recovery_avg / 100), 1)
             self.__apply_correction()
 
     def __apply_correction(self):