32db64630ca42e137fbe736321968ed0aba5c429
AlexECX <33321263+AlexECX@users.noreply.github.com>
2019-08-16 15:15:21 -0400

    Transcrypt: hook to vue build, ignore eslint, update gitignore

---
+++ web/frontend/valexa_app/vue.config.js
@@ -0,0 +1,27 @@
+// vue.config.js
+
+//This will run transcrypt on serve or build
+const child_process = require('child_process');
+const path = require('path')
+
+let cmd_parts = [
+    'python -m transcrypt',
+    '--nomin',
+    '--map',
+    'canvas'
+];
+let cmd_options = {
+    
+    'cwd': path.join(__dirname, "src/ploting"),
+};
+try {
+    let stdout = child_process.execSync(cmd_parts.join(' '), cmd_options).toString();
+} catch (err) {
+    throw Error(err.stdout.toString() + '\n' + err.stderr.toString());
+} 
+  
+module.exports = {
+    configureWebpack: {
+      plugins: []
+    }
+  }
