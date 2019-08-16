1385d90a7ecfef2a25cab409783d25338e63f1ba
AlexECX <33321263+AlexECX@users.noreply.github.com>
2019-08-15 23:32:27 -0400

    PloterData requires name/file and removed some attributes

--- valexa/ploting/ploters.py
+++ valexa/ploting/ploters.py
@@ -10,15 +10,12 @@ from valexa.ploting.plotly.canvas import ProfilePlotCanvas
 class PloterData:
 
     handler = XlsxHandler
-    calib_data = None
-    valid_data = None
     profiles = []
     tolerance_limit = DEFAULT_TOLERANCE
     acceptance_limit = DEFAULT_ACCEPTANCE
 
-    def __init__(self, file: str or File=None, **kwargs):
-        if file:
-            self.load_data(file)
+    def __init__(self, file: str or File, **kwargs):
+        self.load_data(file)
 
         for key, value in kwargs.items():
             if not hasattr(self, key):
@@ -30,13 +27,10 @@ class PloterData:
 
     def load_data(self, file: str or File) -> None:
         handler = self.handler(file)
-        self.calib_data = handler.get_calibration_data()
-        self.valid_data = handler.get_validation_data()
 
         self.profiles = make_profiles(
-            self.calib_data, self.valid_data, 
-            self.tolerance_limit, self.acceptance_limit
+            handler.get_calibration_data(), 
+            handler.get_validation_data(), 
+            self.tolerance_limit, 
+            self.acceptance_limit
         )
-
-    def reset(self):
-        self.__init__()
