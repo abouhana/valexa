b9d7a814a707ad2c490937a8b73954d6be0c2977
AlexECX <33321263+AlexECX@users.noreply.github.com>
2019-08-15 22:41:45 -0400

    Subclass Profile canvases

--- valexa/ploting/mathplotlib/canvas.py
+++ valexa/ploting/mathplotlib/canvas.py
@@ -2,14 +2,15 @@ from matplotlib.figure import Figure
 import mpl_toolkits.axisartist as AA
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg 
 from PyQt5 import QtWidgets
-
-
+       
 class ProfilePlotCanvas:
 
-    def __init__(self, profile, parent=None, width=5, height=5, dpi=100):
-        
+    def __init__(self, profile, **kwargs):
         self.profile = profile
-        self.figure = Figure(figsize=(width, height), dpi=dpi)
+        self.figure = Figure(
+            figsize=(kwargs.pop("width", 5), kwargs.pop("height", 5)), 
+            dpi=kwargs.pop("dpi", 100)
+        )
         self.axes = AA.Subplot(self.figure, 111)
         self.figure.add_subplot(self.axes)
 
@@ -34,47 +35,21 @@ class ProfilePlotCanvas:
         ax.set_xlabel("Concentration")
         ax.set_ylabel("Recovery (%)")
         ax.legend(loc=1)
- 
+
     
-class ProfilePlotCanvasQTAgg(FigureCanvasQTAgg):
+class ProfilePlotCanvasQTAgg(ProfilePlotCanvas, FigureCanvasQTAgg):
 
-    def __init__(self, profile, parent=None, width=5, height=5, dpi=100):
+    def __init__(self, profile, **kwargs):
         
-        self.profile = profile
-        fig = Figure(figsize=(width, height), dpi=dpi)
-        self.axes = AA.Subplot(fig, 111)
-        fig.add_subplot(self.axes)
+        ProfilePlotCanvas.__init__(self, profile, **kwargs)
+        FigureCanvasQTAgg.__init__(self, self.figure)
 
-        super().__init__(fig)
+        self.setParent(kwargs.pop("parent", None))
 
-        self.setParent(parent)
-
-        super().setSizePolicy(
+        self.setSizePolicy(
             QtWidgets.QSizePolicy.Expanding,
             QtWidgets.QSizePolicy.Expanding
         )
 
-        super().updateGeometry()
-
-        self._make_plot()
-
-    def _make_plot(self):
-        p = self.profile
-        ax = self.axes
-        levels_x = [l.calculated_concentration for l in p.levels]
-        ax.axis["bottom", "top", "right"].set_visible(False)
-        ax.axis["y=100"] = ax.new_floating_axis(nth_coord=0, value=100)
-        ax.plot(levels_x, [l.recovery for l in p.levels], color="m", linewidth=2.0, marker=".", label="Recovery")
-        ax.plot(levels_x, [l.rel_tolerance[0] for l in p.levels], linewidth=1.0, color="b",
-                label="Min tolerance limit")
-        ax.plot(levels_x, [l.rel_tolerance[1] for l in p.levels], linewidth=1.0, color="g",
-                label="Max tolerance limit")
-        ax.plot(levels_x, [p.acceptance_interval[0] for _ in p.levels], "k--", label="Acceptance limit")
-        ax.plot(levels_x, [p.acceptance_interval[1] for _ in p.levels], "k--")
-        results_x = [s.concentration for s in p.series]
-        results_y = [(s.result / s.concentration) * 100 for s in p.series]
-        ax.scatter(results_x, results_y, alpha=0.5, s=2)
-        ax.set_xlabel("Concentration")
-        ax.set_ylabel("Recovery (%)")
-        ax.legend(loc=1)
-    
+        self.updateGeometry()
+        
