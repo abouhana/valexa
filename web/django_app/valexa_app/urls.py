7a160a0df97b84d206a101eea129b5b9e97c4f7f
AlexECX <33321263+AlexECX@users.noreply.github.com>
2019-08-15 21:06:41 -0400

    Remove django app and dependencies

--- web/django_app/valexa_app/urls.py
+++
@@ -1,9 +0,0 @@
-"""valexa app URL Configuration"""
-from django.urls import include, path
-
-from . import views
-
-app_name = "valexa_app"
-urlpatterns = [
-    path('compute/', views.ValexaView.as_view(), name="compute"),
-]
