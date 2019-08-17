540207087a03f60972715c97fb6fe3422297cc3d
AlexECX <33321263+AlexECX@users.noreply.github.com>
2019-08-17 16:04:35 -0400

    Change PloterData kwargs to optional positional args

--- valexa/ploting/ploters.py
+++ valexa/ploting/ploters.py
@@ -1,29 +1,22 @@
 import json
 from typing import Dict, List
 
-from valexa.core.profiles import (DEFAULT_ACCEPTANCE, DEFAULT_TOLERANCE,
-    Profile, make_profiles)
+from valexa.core.profiles import (DEFAULT_ACCEPTANCE as ACCEPTANCE, 
+    DEFAULT_TOLERANCE as TOLERANCE, Profile, make_profiles)
 from valexa.core.xlsx import XlsxHandler
 from valexa.ploting.plotly.canvas import ProfilePlotCanvas
 
 
 class PloterData:
 
-    handler = XlsxHandler
-    profiles = []
-    tolerance_limit = DEFAULT_TOLERANCE
-    acceptance_limit = DEFAULT_ACCEPTANCE
+    def __init__(self, file: str or File, handler=XlsxHandler, 
+            tolerance_limit=TOLERANCE, acceptance_limit=ACCEPTANCE):
+        
+        self.tolerance_limit = tolerance_limit
+        self.acceptance_limit = acceptance_limit
+        self.handler = handler
 
-    def __init__(self, file: str or File, **kwargs):
         self.load_data(file)
-
-        for key, value in kwargs.items():
-            if not hasattr(self, key):
-                raise TypeError("%s() received an invalid keyword %r. Only "
-                                "arguments that are already attributes of the "
-                                "class are accepted." % (self.__name__, key))
-            else:
-                setattr(self, key, value)
 
     def load_data(self, file: str or File) -> None:
         handler = self.handler(file)
