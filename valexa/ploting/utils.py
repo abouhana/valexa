2357f52e59da9169c877f898a67e5a3cb61d06d6
AlexECX <33321263+AlexECX@users.noreply.github.com>
2019-08-19 15:47:22 -0400

    Add model name to profile_dict

--- valexa/ploting/utils.py
+++ valexa/ploting/utils.py
@@ -4,6 +4,7 @@ from valexa.core.profiles import Profile
 def profile_to_dict(profile: Profile):
     p = dict(profile.__dict__)
     p["model"] = dict(p["model"].__dict__)
+    p["model"]["name"] = profile.model.name
     p["levels"] = [l.__dict__ for l in p["levels"]]
     p["series"] = [{
         "series": s.series,
