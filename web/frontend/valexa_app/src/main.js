51c6eaa27695dacc114a717cdf92243be77eed04
AlexECX <33321263+AlexECX@users.noreply.github.com>
2019-08-20 15:44:07 -0400

    Introduce VeeValidate

--- web/frontend/valexa_app/src/main.js
+++ web/frontend/valexa_app/src/main.js
@@ -1,8 +1,25 @@
 import Vue from 'vue'
 import App from './App.vue'
 import axios from "axios"
+import VeeValidate from "vee-validate"
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+VeeValidate.Validator.extend('file', (value, [otherValue]) => {
+  // do something and finally return an object like this one:
+  return {
+    valid: false, // or false
+    data: {
+      required: true // or false
+    }
+  };
+}, {
+  computesRequired: true
+})
+
+Vue.use(VeeValidate, {
+  classes: true
+});
 
 axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
